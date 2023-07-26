82536f6d1c555a0248d078faed601d68515523fa
linda_gez <lidiyagez@gmail.com>
2023-07-26 06:52:37 -0800

    react intro

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,20 @@
+import logo from './holberton-logo.jpeg';
+import './App.css';
+
+function App(){
+	return(
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        Copyright {getFullYear()} - {getFooterCopy()}
+      </div>
+    </div>
+  );
+}
+export default App;
