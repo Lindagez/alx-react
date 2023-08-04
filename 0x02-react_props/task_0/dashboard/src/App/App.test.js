39617aab367901b7f23b6d368dc8dd167b002880
linda_gez <lidiyagez@gmail.com>
2023-08-04 08:55:53 -0800

    alx-react

---
+++ 0x02-react_props/task_0/dashboard/src/App/App.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import App from "./App";
+import { shallow } from "enzyme";
+
+describe("App tests", () => {
+  it("renders without crashing", () => {
+    const component = shallow(<App />);
+
+    expect(component).toBeDefined();
+  });
+/*import React from "react";
+import App from "./App";
+import { shallow } from "enzyme";
+
+const app = shallow(<App />);
+describe("App Component Tests", () => {
+  it("Renders without crashing", () => {
+    expect(app).toBeDefined();
+  });
+  it("renders a div with the class App-header", () => {
+    expect(app.find(".App-header")).toBeDefined();
+  });
+  it("renders a div with the class App-body", () => {
+    expect(app.find(".App-body")).toBeDefined();
+  });
+  it("renders a div with the class App-footer", () => {
+    expect(app.find(".App-footer")).toBeDefined();
+  });
+});*/
